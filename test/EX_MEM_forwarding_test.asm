bac42c101c60f57b8c4257ffe92d857623679f33
邢龙 <2435409107@qq.com>
2022-12-14 19:08:55 +0800

    新增用于测试forwarding的asm和hex指令文件

---
+++ test/EX_MEM_forwarding_test.asm
@@ -0,0 +1,14 @@
+.text
+addi t0, zero, 1
+addi t1, t0, 2
+addi t2, t0, 3
+
+# transition
+addi t1, zero, 1
+addi t1, zero, 1
+addi t1, zero, 1
+addi t1, zero, 1
+
+addi t0, zero, 1
+addi t0, t0, 2
+addi t0, t0, 3
