712f3a9355c6d20dffa8a3714f80bd65530cf24f
邢龙 <2435409107@qq.com>
2023-02-03 19:24:18 +0800

    新增分支预测测试用例

---
+++ test/branch_hazard_test.asm
@@ -0,0 +1,16 @@
+.text
+addi t1, zero, 1
+addi t2, zero, 3
+
+for_loop:
+	addi t1, t1, 1
+	
+	addi t4, zero, 15
+	addi t4, zero, 15
+	
+	blt t1, t2, for_loop
+	
+for_loop_return:
+addi t4, zero, 15
+
+addi zero, zero, 0
---
+++ test/load_use_hazard_test.asm
@@ -0,0 +1,12 @@
+.text
+addi t0, zero, 6
+sw t0, 0(sp)
+
+lw t1, 0(sp)
+addi t2, t1, 1
+
+addi t3, zero, 10
+
+addi t4,zero, 1
+addi t4, zero, 2
+addi t4, zero, 3
